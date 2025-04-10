3d3a61963efe2008378bb154641f96d6e3e9218b
Pawan Kumar <pawank4285@gmail.com>
2025-04-10 16:15:32 +0530

    Commit sql scripts

---
+++ foodHub.Repository/SQLScripts/SeedData.sql
@@ -0,0 +1,7 @@
+SELECT * from Categories
+SELECT * from ItemTypes
+
+-- Insert the data for item types and categories
+insert into ItemTypes values('Veg'),('Non-Veg');
+
+insert into Categories values('Pizza','Pizza'),('Dessert','Dessert'),('Beverages','Beverages');
